854b1b8beb06dd906600236d8c8cce4a2a999780
utevo666 <147385556+utevo666@users.noreply.github.com>
2023-11-13 14:41:58 +0100

    Update zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -109,31 +109,9 @@ print(wynik)
 
 
 
-def ile_ujemnych(lista):
-    count = 0
-    for element in lista:
-        if element < 0:
-            count += 1
-    return count
+napis = "abrakadabra"
+usuwany = "ab"
+def remove(napis, usuwany):
+    print(napis.replace(usuwany,"",1))
 
-lista = [-1,-2,-5,-6,1,2,5]
-print(f"liczba ujemnych elementow: {ile_ujemnych(lista)}")
-
-def czy_nalezy(lista, element):
-    return element in lista
-
-moja_lista = [5, 3, 1, 4, 2]
-element_do_sprawdzenia = 4
-
-wynik = czy_nalezy(moja_lista, element_do_sprawdzenia)
-print(wynik)
-
-def czy_zawiera(lista1, lista2):
-    return all(element in lista1 for element in lista2)
-
-
-lista1 = [5, 3, 1, 4]
-lista2 = [3, 2]
-
-wynik = czy_zawiera(lista1, lista2)
-print(wynik)
+remove(napis,usuwany)
